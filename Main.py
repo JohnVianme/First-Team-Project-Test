c96f2558d2d6732b4c6e1c6ec5ecb7176b989b3d
LXL1103 <114650659+LXL1103@users.noreply.github.com>
2024-06-18 21:14:25 -0700

    Root Added

--- Main.py
+++ Main.py
@@ -14,6 +14,11 @@ def division(num1,num2):
 def multiplication(num1, num2):
 	return num1 * num2
 
+def Nth_Root(num,Nth):
+       return num**(1/Nth)
+def Sqr_Root(num):
+       return num**(1/2)
 #print statement to test function works
 print(addition(5 , 5))
+print(Sqr_Root(4))
 
