25d8fba4b82219a00f2df67b12c0f40f92c5548a
LXL1103 <114650659+LXL1103@users.noreply.github.com>
2024-06-19 19:59:58 -0700

    Box Added

--- Main.py
+++ Main.py
@@ -1,11 +1,36 @@
 # This is the main file for the calc app
 # Authors: John and Man
 from tkinter import *
+import tkinter as tk
 
-main = Tk()
-main.mainloop()
+expression = ""
+if __name__ == "__main__": 
+      gui = Tk() 
 
+      
+      
+      
+      # set the background colour of GUI window 
+      gui.configure(background="black") 
+      
+      # set the title of GUI window 
+      gui.title("Calculator") 
+      
+      # set the configuration of GUI window 
+      gui.geometry("400x600")
 
+      equation = StringVar() 
+ 
+      # create the text entry box for 
+      # showing the expression . 
+      expression_field = Entry(gui, textvariable=equation) 
+      
+      # grid method is used for placing 
+      # the widgets at respective positions 
+      # in table like structure . 
+      expression_field.grid(columnspan= 90, ipadx=110)
+
+      gui.mainloop() 
 
 # addition fuction, takes in two inputs numbers and adds them
 def addition(num1,num2):
@@ -31,7 +56,7 @@ def exponential (num, power):
 #print statement to test function works
 #print(addition(5 , 5))
 #print(Sqr_Root(4))
-print("buttttttttt")
+#print("buttttttttt")
 
 
 
